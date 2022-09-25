b9d4588e1be7f9c6f5cb37b6b989635399a7e3b8
moschd <86148557+moschd@users.noreply.github.com>
2022-09-25 19:34:14 +0200

    runninglocal

--- main.cpp
+++ main.cpp
@@ -5,25 +5,20 @@
 #include <chrono>
 #include <cmath>
 #include <regex>
-#include <jsoncpp/json/json.h>
 #include <ext/pb_ds/assoc_container.hpp>
-
+#include "dist/json/json.h"
 #include "constants.h"
 #include "geometry.h"
-
 #include "kd-tree.h"
 #include "geometricShape.h"
-
 #include "item.h"
 #include "itemSortMethods.h"
 #include "itemregister.h"
-
 #include "calculationCache.h"
 #include "gravity.h"
 #include "bin.h"
 #include "packingCluster.h"
 #include "packer.h"
-
 #include "binSortMethods.h"
 #include "distributor.h"
 #include "outgoingJsonBuilder.h"
@@ -38,16 +33,11 @@
         JSON
 */
 
-// extern "C"
-// {
-//     char *packToBinAlgorithm(char *result,
-//                              const int bufferSize,
-//                              const char *incomingJson,
-//                              const bool includeBins = 1,
-//                              const bool includeItems = 1,
-//                              const bool itemDimensionsAfter = 0,
-//                              const int responsePrecision = 7)
-//     {
+#ifndef RUNNING_LOCAL
+#define RUNNING_LOCAL 0
+#endif
+
+#if RUNNING_LOCAL
 
 int main()
 {
@@ -57,14 +47,27 @@ int main()
     const bool includeBins = 1;
     const int responsePrecision = 7;
 
-    // std::ifstream incomingJson("/home/moschd/pomaster/packingOptimizerAlgos/packToBin/testfiles/cpp_vs_python_10000_items.json");
+    std::ifstream incomingJson("/home/moschd/pomaster/packingOptimizerAlgos/packToBin/testfiles/cpp_vs_python_10000_items.json");
     // std::ifstream incomingJson("/home/moschd/pomaster/packingOptimizerAlgos/packToBin/testfiles/nullutil.json");
-    std::ifstream incomingJson("/home/moschd/pomaster/packingOptimizerAlgos/packToBin/testfiles/testjson.json");
+    // std::ifstream incomingJson("/home/moschd/pomaster/packingOptimizerAlgos/packToBin/testfiles/testjson.json");
     // std::ifstream incomingJson("/home/moschd/pomaster/packingOptimizerAlgos/packToBin/testfiles/testing2.json");
     // std::ifstream incomingJson("/home/moschd/pomaster/packingOptimizerAlgos/packToBin/testfiles/cpp_vs_python_1000_items.json");
     // std::ifstream incomingJson ("/home/moschd/packingOptimizerAlgos/packToBin/testfiles/cpp_vs_python_2500_items.json");
     // std::ifstream incomingJson("/home/moschd/pomaster/packingOptimizerAlgos/packToBin/testfiles/cpp_vs_python_5000_items.json");
     // std::ifstream incomingJson("/home/moschd/pomaster/packingOptimizerAlgos/packToBin/testfiles/5000_2500_dim.json");
+
+#else
+extern "C"
+{
+    char *packToBinAlgorithm(char *result,
+                             const int bufferSize,
+                             const char *incomingJson,
+                             const bool includeBins = 1,
+                             const bool includeItems = 1,
+                             const bool itemDimensionsAfter = 0,
+                             const int responsePrecision = 7)
+    {
+#endif
 
     Json::Reader reader;
     Json::Value inboundRoot;
@@ -129,27 +132,29 @@ int main()
     ResponseBuilder outgoingJsonBuilder(responsePrecision, includeBins, includeItems, itemDimensionsAfter);
     outgoingJsonBuilder.generate(packingProcessor);
 
+#if RUNNING_LOCAL
     std::ofstream myfile;
     myfile.open("ex1.json");
     std::string output = Json::writeString(outgoingJsonBuilder.getBuilder(), outgoingJsonBuilder.getMessage());
     myfile << output;
     myfile.close();
-
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     std::cout << duration.count() << std::endl;
-
     return 1;
-
-    //     std::string resultJson(Json::writeString(outgoingJsonBuilder.getBuilder(), outgoingJsonBuilder.getMessage()));
-    //     if (resultJson.size() < bufferSize)
-    //     {
-    //         resultJson.copy(result, bufferSize);
-    //         return result;
-    //     }
-    //     else
-    //     {
-    //         return 0;
-    //     }
-    // };
 };
+
+#else
+        std::string resultJson(Json::writeString(outgoingJsonBuilder.getBuilder(), outgoingJsonBuilder.getMessage()));
+        if (resultJson.size() < bufferSize)
+        {
+            resultJson.copy(result, bufferSize);
+            return result;
+        }
+        else
+        {
+            return 0;
+        }
+    };
+};
+#endif
